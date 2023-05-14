1eed8b944881de60808d4c1e7c590d3ec09fa96a
David Kwong <20544dk@gmail.com>
2023-05-14 03:37:14 +0000

    Stricten print table parameters

--- rust/print_table/src/lib.rs
+++ rust/print_table/src/lib.rs
@@ -1,21 +1,14 @@
-pub fn print_table<T>(data: &[&[T]], headers: &[&str]) -> Vec<String>
+pub fn print_table<T, const K: usize>(data: &[[T; K]], headers: &[&str; K]) -> Vec<String>
 where
     T: ToString,
 {
-    let mut num_col = 0;
-    let mut col_width = Vec::<u32>::new();
+    let mut col_width = [0; K];
     let num_row = data.len();
 
     for row in data {
-        if row.len() > num_col {
-            num_col = row.len();
-        }
-
         for (i, col) in row.iter().enumerate() {
             let len = col.to_string().len() as u32;
-            if col_width.len() <= i {
-                col_width.push(len);
-            } else if col_width[i] < len {
+            if col_width[i] < len {
                 col_width[i] = len;
             }
         }
@@ -23,18 +16,25 @@ where
 
     for (i, header) in headers.iter().enumerate() {
         let len = header.len() as u32;
-        let current_col_width = col_width.get(i).unwrap_or(&len);
-        if current_col_width < &len {
+        let current_col_width = col_width[i];
+        if current_col_width < len {
             col_width[i] = len;
         }
     }
 
-    let mut table = Vec::<String>::with_capacity(num_row + 3);
+    let mut table = Vec::<String>::with_capacity(num_row + 4);
 
-    let mut header_border = String::new();
-    let mut header = String::new();
+    let num_col = headers.len();
 
-    for i in 0..num_col {
+    let column_width_sum = col_width.iter().sum::<u32>() as usize;
+    let padding = num_col * 2;
+    let borders = num_col + 1;
+    let table_width = column_width_sum + padding + borders;
+
+    let mut header_border = String::with_capacity(table_width);
+    let mut header = String::with_capacity(table_width);
+
+    for i in 0..K {
         let len = *(col_width.get(i).unwrap_or(&0)) as usize + 2;
         let border_col = format!("+{:-<padding$}", "", padding = len);
         header_border.push_str(&border_col);
@@ -50,7 +50,7 @@ where
     table.push(header_border.clone());
 
     for row in data.iter() {
-        let mut row_string = String::new();
+        let mut row_string = String::with_capacity(table_width);
         for i in 0..num_col {
             let len = *(col_width.get(i).unwrap_or(&0)) as usize + 2;
             let data_value = row.get(i).map(|e| e.to_string()).unwrap_or("".to_string());
@@ -66,19 +66,14 @@ where
 #[cfg(test)]
 #[test]
 fn test_table() {
-    let sample_data = vec![
-        vec!["Abel", "34", "male"],
-        vec!["Tong tong wowwwww such long length", "34", "female"],
-        vec!["Casper", "25", "male"],
+    let sample_data: Vec<[&str; 3]> = vec![
+        ["Abel", "34", "male"],
+        ["Tong tong wowwwww such long length", "34", "female"],
+        ["Casper", "25", "male"],
     ];
 
-    let mut sample_ref = Vec::with_capacity(3);
-
-    for row in sample_data.iter() {
-        sample_ref.push(row.as_slice());
-    }
-    let headers = vec!["Name", "Points", "Gender"];
-    let table = print_table(&sample_ref, &headers);
+    let headers = ["Name", "Points", "Gender"];
+    let table = print_table(&sample_data, &headers);
 
     // Adds new line at the end, because sample output contains a new line at the end of the file
     let generated_output = format!("{}\n", table.join("\n"));
