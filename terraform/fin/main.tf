49f7f533e7fc0767d40313654fefa5060c28779a
David Kwong <20544dk@gmail.com>
2023-07-27 10:42:53 +0000

    Add fin api tables

--- terraform/fin/main.tf
+++ terraform/fin/main.tf
@@ -20,5 +20,32 @@ resource "aws_dynamodb_table" "classifying_rules_table" {
   }
 }
 
+resource "aws_dynamodb_table" "unprocessed_transactions_table" {
+  name            = "fin_unprocessed_transactions_${var.environment}"
+  billing_mode    = "PAY_PER_REQUEST"
+  hash_key        = "id"
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
+}
+
+resource "aws_dynamodb_table" "transactions_table" {
+  name            = "fin_transactions_${var.environment}"
+  billing_mode    = "PAY_PER_REQUEST"
+  hash_key        = "month"
+  range_key       = "date"
+
+  attribute {
+    name = "month"
+    type = "N"
+  }
+
+  attribute {
+    name = "date"
+    type = "N"
+  }
+}
 
 
