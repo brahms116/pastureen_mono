4b9af1d241063b5f99a895a9d4d6d2d58c83d850
David Kwong <20544dk@gmail.com>
2023-07-31 21:51:36 +0000

    Initial impl in cli

--- rust/fin_cli/src/main.rs
+++ rust/fin_cli/src/main.rs
@@ -1,14 +1,32 @@
+use chrono::NaiveDate;
 use clap::{Args, Parser, Subcommand};
+use csv_iterator::*;
 use fin_api::*;
 
 #[derive(Parser, Debug)]
 struct CliArgs {
     #[clap(subcommand)]
-    command: Option<FinSubcommand>,
+    command: FinSubcommand,
 }
 
 #[derive(Subcommand, Debug)]
 enum FinSubcommand {
+    Report {
+        #[clap(long)]
+        from: String,
+        #[clap(long)]
+        to: String,
+    },
+    List {
+        #[clap(long)]
+        from: String,
+        #[clap(long)]
+        to: String,
+    },
+    Import {
+        #[clap(long)]
+        path: String,
+    },
     Process,
     Unprocessed,
     Types(TransactionTypes),
@@ -77,6 +95,42 @@ fn handle_error<T: std::error::Error>(error: T) {
     println!("Oops something went wrong: {:?}", error);
 }
 
+fn print_report(report: &Report) {
+    println!("Report from {} to {}", report.start_date, report.end_date);
+    println!("");
+    let headers: [&str; 2] = ["Transaction Type", "Amount"];
+    let rows: Vec<[String; 2]> = report
+        .by_type
+        .iter()
+        .map(|(transaction_type, amount)| [transaction_type.to_string(), amount.to_string()])
+        .collect();
+    let output_rows = print_table::print_table(&rows, &headers);
+    for row in output_rows {
+        println!("{}", row);
+    }
+    println!("");
+    println!("Total: {}", report.total);
+}
+
+fn transaction_to_row(transaction: &Transaction) -> [String; 5] {
+    [
+        transaction.id.to_string(),
+        transaction.date.to_string(),
+        transaction.amount_cents.to_string(),
+        transaction.description.to_string(),
+        transaction.transaction_type_id.to_string(),
+    ]
+}
+
+fn print_transactions(transactions: &[Transaction]) {
+    let headers: [&str; 5] = ["ID", "Date", "Amount", "Description", "Transaction Type ID"];
+    let rows: Vec<[String; 5]> = transactions.iter().map(transaction_to_row).collect();
+    let output_rows = print_table::print_table(&rows, &headers);
+    for row in output_rows {
+        println!("{}", row);
+    }
+}
+
 fn classifying_rules_to_row(classifying_rule: &ClassifyingRule) -> [String; 4] {
     [
         classifying_rule.id.to_string(),
@@ -115,6 +169,92 @@ fn print_transaction_types(transaction_types: &[TransactionType]) {
     for row in output_rows {
         println!("{}", row);
     }
+}
+
+async fn report<T: FinApi>(api: T, from: String, to: String) {
+    let from = NaiveDate::parse_from_str(&from, "%d/%m/%Y");
+    if let Err(e) = from {
+        println!("Could not parse from date: {}", e);
+        return;
+    }
+    let from = from
+        .expect("Should handle error case")
+        .and_hms_opt(0, 0, 0)
+        .unwrap();
+
+    let to = NaiveDate::parse_from_str(&to, "%d/%m/%Y");
+    if let Err(e) = to {
+        println!("Could not parse to date: {}", e);
+        return;
+    }
+    let to = to
+        .expect("Should handle error case")
+        .and_hms_opt(0, 0, 0)
+        .unwrap();
+
+    let result = api.generate_report(from.timestamp(), to.timestamp()).await;
+    if let Err(e) = result {
+        handle_error(e);
+        return;
+    }
+    let report = result.expect("Should handle error case");
+    print_report(&report);
+}
+
+async fn list<T: FinApi>(api: T, from: String, to: String) {
+    let from = NaiveDate::parse_from_str(&from, "%d/%m/%Y");
+    if let Err(e) = from {
+        println!("Could not parse from date: {}", e);
+        return;
+    }
+    let from = from
+        .expect("Should handle error case")
+        .and_hms_opt(0, 0, 0)
+        .unwrap();
+
+    let to = NaiveDate::parse_from_str(&to, "%d/%m/%Y");
+    if let Err(e) = to {
+        println!("Could not parse to date: {}", e);
+        return;
+    }
+    let to = to
+        .expect("Should handle error case")
+        .and_hms_opt(0, 0, 0)
+        .unwrap();
+
+    let result = api
+        .list_transactions(from.timestamp(), to.timestamp(), None)
+        .await;
+
+    if let Err(e) = result {
+        handle_error(e);
+        return;
+    }
+    let result = result.expect("Should handle error case");
+    print_transactions(&result);
+}
+
+async fn import<T: FinApi>(api: T, path: String) {
+    let iter = CsvIterator::<INGTransaction>::new(&path);
+    if iter.is_err() {
+        println!("Could not read file: {}", path);
+    }
+    let mut iter = iter.expect("Should handle error case");
+    let mut transactions = Vec::new();
+    while let Some(transaction) = iter.next() {
+        if let Ok(transaction) = transaction {
+            transactions.push(transaction);
+        } else {
+            println!("Could not parse transaction {:?}", transaction);
+        }
+    }
+    let result = api.process_ing_transactions(&transactions).await;
+    if let Err(e) = result {
+        handle_error(e);
+        return;
+    }
+    let result = result.expect("Should hanle error case");
+    println!("{result}")
 }
 
 async fn classifying_rules<T: FinApi>(api: T, args: ClassifyingRules) {
@@ -272,11 +412,7 @@ async fn main() {
 
     let api = FinApiService::new(db);
 
-    if let None = args.command {
-        println!("No command provided");
-        return;
-    }
-    let command = args.command.expect("Should handle none case above");
+    let command = args.command;
 
     match command {
         FinSubcommand::Process => {
@@ -290,6 +426,15 @@ async fn main() {
         }
         FinSubcommand::Rules(args) => {
             classifying_rules(api, args).await;
+        }
+        FinSubcommand::Import { path } => {
+            import(api, path).await;
+        }
+        FinSubcommand::List { from, to } => {
+            list(api, from, to).await;
+        }
+        FinSubcommand::Report { from, to } => {
+            report(api, from, to).await;
         }
     }
 }
