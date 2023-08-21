dc4d03c90d9ffc4d135ed921f7538881fee969fd
David Kwong <20544dk@gmail.com>
2023-08-21 11:37:58 +0000

    Add prod and test envs

--- rust/auth_web_service/terraform/main.tf
+++ rust/auth_web_service/terraform/main.tf
@@ -2,3 +2,13 @@ module "dev" {
   source      = "./module"
   environment = "dev"
 }
+
+module "test" {
+  source      = "./module"
+  environment = "test"
+}
+
+module "prod" {
+  source      = "./module"
+  environment = "prod"
+}
