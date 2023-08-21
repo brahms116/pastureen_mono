1c7b1310d5d79bd997d083f76d17cc16b725543c
David Kwong <20544dk@gmail.com>
2023-08-21 11:37:11 +0000

    Fix deploy script

--- rust/auth_web_service/deploy.sh
+++ rust/auth_web_service/deploy.sh
@@ -4,51 +4,19 @@ set -e
 
 env=$1
 
-if [[ "$env" != "prod" && "$env" != "dev"  ]]; then
-  echo "Please provide a valid environment name (prod or dev)"
-    exit 1
+if [[ "$env" != "prod" && "$env" != "dev" && "$env" != "test"]]; then
+  echo "Please provide a valid environment name (prod, test or dev)"
+  exit 1
 fi
 
 echo "Compiling"
 
 docker run -v "$(pwd)/../":/app -v .:/out --env PROJECT_NAME=auth_web_service public.ecr.aws/p1r0g3x6/rust_lambda_build_container:latest
 
+echo "Deploying to $env"
 
-if [[ "$env" == "prod" ]]; then
-  echo "Deploying to production environment"
-  set -a
-  . ../.env
-  set +a
-  aws lambda update-function-code --function-name auth_service_prod --zip-file fileb://./lambda.zip
-  aws lambda update-function-configuration --function-name auth_service_prod --environment \
-    Variables="{\
-      AUTH_SERVICE_SECRET=$AUTH_SERVICE_SECRET,\
-      AUTH_SERVICE_DB_CONN_STR=$AUTH_SERVICE_DB_CONN_STR,\
-      AUTH_WEB_SERVICE_LISTEN_ADDR=$AUTH_WEB_SERVICE_LISTEN_ADDR\
-    }"
-
-  exit 0
-fi
-
-echo "Deploying to development environment"
-set -a
-. ../.dev.env
-set +a
-aws lambda update-function-code --function-name auth_service_dev --zip-file fileb://./lambda.zip
-aws lambda update-function-configuration --function-name auth_service_dev --environment \
-  Variables="{\
-    AUTH_SERVICE_SECRET=$AUTH_SERVICE_SECRET,\
-    AUTH_SERVICE_DB_CONN_STR=$AUTH_SERVICE_DB_CONN_STR,\
-    AUTH_WEB_SERVICE_LISTEN_ADDR=$AUTH_WEB_SERVICE_LISTEN_ADDR\
-  }"
-
-
-echo "Deploying to test environment"
-set -a
-. ../.test.env
-set +a
-aws lambda update-function-code --function-name auth_service_test --zip-file fileb://./lambda.zip
-aws lambda update-function-configuration --function-name auth_service_test --environment \
+aws lambda update-function-code --function-name auth_service_$env --zip-file fileb://./lambda.zip
+aws lambda update-function-configuration --function-name auth_service_$env --environment \
   Variables="{\
     AUTH_SERVICE_SECRET=$AUTH_SERVICE_SECRET,\
     AUTH_SERVICE_DB_CONN_STR=$AUTH_SERVICE_DB_CONN_STR,\
