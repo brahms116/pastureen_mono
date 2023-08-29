41391951aed222b1fbbebc139c63348a8632f57e
David Kwong <20544dk@gmail.com>
2023-08-29 07:56:28 +0000

    Topbar props

--- golang/design/main.go
+++ golang/design/main.go
@@ -8,12 +8,53 @@ import "embed"
 import "html/template"
 import "net/http"
 
+type LogoProps struct {
+	LogoText string
+	LogoLink string
+}
+
+type NavItem struct {
+	Link string
+	Text string
+	Id   string
+}
+
+type TopBarProps struct {
+	LogoProps     LogoProps
+	NavItems      []NavItem
+	MenuOpen      bool
+	CurrentPageId string
+}
+
 type LayoutProps struct {
-  Title string
+	Title string
 }
 
 type IndexProps struct {
-  LayoutProps LayoutProps
+	LayoutProps LayoutProps
+}
+
+func GetTopBarProps (pageId string) (TopBarProps) {
+  return TopBarProps{
+    LogoProps: LogoProps{
+      LogoText: "Logo",
+      LogoLink: "/",
+    },
+    NavItems: []NavItem{
+      {
+        Link: "/",
+        Text: "Home",
+        Id: "home",
+      },
+      {
+        Link: "/about",
+        Text: "About",
+        Id: "about",
+      },
+    },
+    MenuOpen: false,
+    CurrentPageId: pageId,
+  }
 }
 
 //go:embed templates/*
@@ -25,18 +66,18 @@ var assetsFS embed.FS
 func main() {
 	r := gin.Default()
 
-  html := template.Must(template.ParseFS(f, "templates/**/*"))
-  r.SetHTMLTemplate(html)
+	html := template.Must(template.ParseFS(f, "templates/**/*"))
+	r.SetHTMLTemplate(html)
 
-  r.StaticFS("/static", http.FS(assetsFS))
+	r.StaticFS("/static", http.FS(assetsFS))
 
-  r.GET("/", func (c *gin.Context) {
-    c.HTML(http.StatusOK, "index.html", IndexProps{
-      LayoutProps: LayoutProps{
-        Title: "Hello World",
-      },
-    })
-  })
+	r.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", IndexProps{
+			LayoutProps: LayoutProps{
+				Title: "Hello World",
+			},
+		})
+	})
 
 	r.Run()
 }
