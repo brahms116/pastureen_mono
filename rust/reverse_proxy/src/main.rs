726217c8c812e8c33a3b18208459b9a41f092ab4
David Kwong <20544dk@gmail.com>
2023-09-13 11:54:52 +0000

    Fix up reverse_proxy

--- rust/reverse_proxy/src/main.rs
+++ rust/reverse_proxy/src/main.rs
@@ -1,12 +1,25 @@
-use hyper::{Body, Request, Response, Server, http::request::Parts, Uri};
+use hyper::{
+    http::request::Parts, http::uri::InvalidUri, Body, Client, Request, Response, Server, Uri,
+};
 use std::net::SocketAddr;
 use thiserror::Error;
+
+/* ERRORS */
+
+type StdError = Box<dyn std::error::Error + Send + Sync + 'static>;
 
 #[derive(Error, Debug)]
 pub enum ReverseProxyError {
     #[error("Missing environment variable: {0}")]
     MissingConfiguration(String),
+    #[error("Invalid Uri: {0:?}")]
+    InvalidUri(#[from] InvalidUri),
+    #[error("Failed to send proxy request: {0:?}")]
+    ProxyRequestError(#[from] hyper::Error),
 }
+
+
+/* CONFIG */
 
 pub struct ReverseProxyConfig {
     pub listen_addr: String,
@@ -28,57 +41,13 @@ impl ReverseProxyConfig {
     }
 }
 
-type Error = Box<dyn std::error::Error + Send + Sync + 'static>;
 
-#[tokio::main]
-async fn main() -> Result<(), Error> {
-    let config = ReverseProxyConfig::from_env()?;
-    let addr: SocketAddr = config.listen_addr.parse()?;
-
-    let service_fn = hyper::service::make_service_fn(|_| async {
-        Ok::<_, Error>(hyper::service::service_fn(reverse_proxy))
-    });
-
-    let server = Server::bind(&addr).serve(service_fn);
-
-    if let Err(e) = server.await {
-        eprintln!("server error: {}", e);
-    }
-
-    Ok(())
-}
+/* ROUTE */
 
 pub enum Route {
     DesignSystem(String),
     NotFound,
     HealthCheck,
-}
-
-fn matches_path(path: &str, route_path: &str) -> bool {
-    let route_path_len = route_path.len();
-
-    if path.len() < route_path_len {
-        return false;
-    }
-
-    if path.starts_with(route_path) {
-        if let Some(next_char) = path.chars().nth(route_path_len) {
-            return match next_char {
-                '/' | '?' => true,
-                _ => false,
-            };
-        }
-        // Perfect match
-        return true;
-    }
-    false
-}
-
-fn strip_prefix(input: &str, prefix: &str) -> String {
-    if input.starts_with(prefix) {
-        return input[prefix.len()..].to_string();
-    }
-    input.to_string()
 }
 
 impl From<&str> for Route {
@@ -109,27 +78,179 @@ impl From<&Parts> for Route {
     }
 }
 
+/* ROUTE_HELPERS */
+
+fn matches_path(path: &str, route_path: &str) -> bool {
+    let route_path_len = route_path.len();
+
+    if path.len() < route_path_len {
+        return false;
+    }
+
+    if path.starts_with(route_path) {
+        if let Some(next_char) = path.chars().nth(route_path_len) {
+            return match next_char {
+                '/' => true,
+                _ => false,
+            };
+        }
+        // Perfect match
+        return true;
+    }
+    false
+}
+
+fn strip_prefix(input: &str, prefix: &str) -> String {
+    if input.starts_with(prefix) {
+        return input[prefix.len()..].to_string();
+    }
+    input.to_string()
+}
+
+
+/* PROXY_ROUTE */
 
 pub enum ProxyRoute {
     DesignSystem(String),
 }
+
+impl ProxyRoute {
+    pub fn proxied_path_and_query(&self) -> String {
+        get_proxied_path_and_query(self)
+    }
+}
+
+/* PROXY_ROUTE_HELPERS */
+
+fn get_proxied_path_and_query(route: &ProxyRoute) -> String {
+    match route {
+        ProxyRoute::DesignSystem(slug) => format!("{}{}", "/design", slug),
+    }
+}
+
+
+/* NON_PROXY_ROUTE */
 
 pub enum NonProxyRoute {
     NotFound,
     HealthCheck,
 }
 
+
+/* CLASSIFIED_ROUTE */
+
 pub enum ClassifiedRoute {
     Proxy(ProxyRoute),
     NonProxy(NonProxyRoute),
 }
 
-async fn reverse_proxy(_req: Request<Body>) -> Result<Response<Body>, Error> {
-    let response = Response::builder()
+impl From<Route> for ClassifiedRoute {
+    fn from(route: Route) -> Self {
+        match route {
+            Route::DesignSystem(slug) => ClassifiedRoute::Proxy(ProxyRoute::DesignSystem(slug)),
+            Route::NotFound => ClassifiedRoute::NonProxy(NonProxyRoute::NotFound),
+            Route::HealthCheck => ClassifiedRoute::NonProxy(NonProxyRoute::HealthCheck),
+        }
+    }
+}
+
+impl From<&Parts> for ClassifiedRoute {
+    fn from(parts: &Parts) -> Self {
+        Route::from(parts).into()
+    }
+}
+
+impl From<&Uri> for ClassifiedRoute {
+    fn from(uri: &Uri) -> Self {
+        Route::from(uri).into()
+    }
+}
+
+/* GET_PROXIED_REQUEST */
+
+fn get_proxied_request(
+    request: Request<Body>,
+    proxied_path_and_query: &str,
+) -> Result<Request<Body>, ReverseProxyError> {
+    let (mut parts, body) = request.into_parts();
+    let query = parts.uri.query().unwrap_or("");
+    let proxied_uri = format!("{}?{}", proxied_path_and_query, query);
+
+    let mut uri_parts = parts.uri.into_parts();
+    uri_parts.path_and_query = Some(proxied_uri.parse()?);
+    parts.uri = Uri::from_parts(uri_parts).expect("Failed to build proxied uri");
+    Ok(Request::from_parts(parts, body))
+}
+
+/* HANDLE_NON_PROXY_ROUTE */
+
+fn handle_non_proxy_route(route: NonProxyRoute) -> Response<Body> {
+    match route {
+        NonProxyRoute::NotFound => not_found_route(),
+        NonProxyRoute::HealthCheck => healthcheck_route(),
+    }
+}
+
+/* HANDLE_PROXY_ROUTE_HELPERS */
+
+fn healthcheck_route() -> Response<Body> {
+    Response::builder()
         .status(200)
         .header("content-type", "text/html")
-        .body(Body::from("Hello World"))
-        .unwrap();
+        .body(Body::from("OK"))
+        .expect("Failed to build healthcheck response")
+}
 
+fn not_found_route() -> Response<Body> {
+    Response::builder()
+        .status(404)
+        .header("content-type", "text/html")
+        .body(Body::from("Not Found"))
+        .expect("Failed to build not found response")
+}
+
+/* SEND_REQUEST */
+
+async fn send_request(request: Request<Body>) -> Result<Response<Body>, ReverseProxyError> {
+    let client = Client::new();
+    let response = client.request(request).await?;
     Ok(response)
+}
+
+
+/* REVERSE_PROXY_FUNCTION */
+
+async fn reverse_proxy(req: Request<Body>) -> Result<Response<Body>, StdError> {
+    let route = ClassifiedRoute::from(req.uri());
+
+    match route {
+        ClassifiedRoute::Proxy(proxy_route) => {
+            let proxied_request = get_proxied_request(req, &get_proxied_path_and_query(&proxy_route))?;
+            let proxied_response = send_request(proxied_request).await?;
+            Ok(proxied_response)
+        }
+        ClassifiedRoute::NonProxy(non_proxy_route) => {
+            let response = handle_non_proxy_route(non_proxy_route);
+            Ok(response)
+        }
+    }
+}
+
+/* BOOTSTRAP / DRIVER */
+
+#[tokio::main]
+async fn main() -> Result<(), StdError> {
+    let config = ReverseProxyConfig::from_env()?;
+    let addr: SocketAddr = config.listen_addr.parse()?;
+
+    let service_fn = hyper::service::make_service_fn(|_| async {
+        Ok::<_, StdError>(hyper::service::service_fn(reverse_proxy))
+    });
+
+    let server = Server::bind(&addr).serve(service_fn);
+
+    if let Err(e) = server.await {
+        eprintln!("server error: {}", e);
+    }
+    Ok(())
 }
