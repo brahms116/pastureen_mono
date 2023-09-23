17f6435b9dd24f91d13ab88d3fe3cd85f5e8a0dc
David Kwong <20544dk@gmail.com>
2023-09-23 05:07:38 +0000

    Yucky javascript

--- rust/blog_site/posts.js
+++ rust/blog_site/posts.js
@@ -3,7 +3,7 @@ function main() {
   const queryParams = new URLSearchParams(window.location.search);
   const search = queryParams.get("search");
 
-  if (search) {
+  if (search && searchInput && htmx) {
     searchInput.value = search;
     htmx.trigger('#search', 'customLoad');
   }
