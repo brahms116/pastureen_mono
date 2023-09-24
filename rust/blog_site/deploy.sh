0b613f96d2a1193ea50f455c2e2c8c12652f7d9c
David Kwong <20544dk@gmail.com>
2023-09-24 01:02:30 +0000

    Deploy blog site yaml action

---
+++ rust/blog_site/deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+env=$1
+
+if [[ "$env" != "prod" && "$env" != "dev" && "$env" != "test" ]]; then
+  echo "Please provide a valid environment name (prod, test or dev)"
+  exit 1
+fi
+
+echo "Uploading blog skeleton to S3 $env"
+
+aws s3 sync ./build/index.html s3://pastureen-blog-$env/index.html --delete
+aws s3 sync ./build/posts.html s3://pastureen-blog-$env/posts.html --delete
