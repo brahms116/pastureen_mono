cc3116954ad171dc5f31f97b0fc187a0167c4d80
David Kwong <20544dk@gmail.com>
2023-09-27 08:15:43 +0000

    Last ditch try with grpc

--- rust/auth_service/src/main.rs
+++ rust/auth_service/src/main.rs
@@ -1,3 +1,11 @@
+use std::net::SocketAddr;
+
+use hyper:: {
+    Response as HyperResponse,
+    Request as HyperRequest,
+    Body
+};
+
 use tonic::{transport::Server, Request, Response, Status};
 
 use hello_world::greeter_server::{Greeter, GreeterServer};
@@ -27,10 +35,28 @@ impl Greeter for MyGreeter {
     }
 }
 
+async fn health_check(_req: HyperRequest<Body>) -> Result<HyperResponse<Body>, StdError> {
+    Ok(HyperResponse::new(Body::from("OK")))
+}
+
+type StdError = Box<dyn std::error::Error + Send + Sync + 'static>;
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let addr = "0.0.0.0:8080".parse()?;
+    let addr = "127.0.0.1:8080".parse()?;
     let greeter = MyGreeter::default();
+
+    let health_addr: SocketAddr = "127.0.0.1:8081".parse()?;
+    let service_fn = hyper::service::make_service_fn(|_| async {
+        Ok::<_, StdError>(hyper::service::service_fn(health_check))
+    });
+
+    let server = hyper::Server::bind(&health_addr).serve(service_fn);
+
+    if let Err(e) = server.await {
+        eprintln!("server error: {}", e);
+    }
+
 
     Server::builder()
         .add_service(GreeterServer::new(greeter))
