f236d2e3d6a9183009c7c051bec4e9a755c1f496
David Kwong <20544dk@gmail.com>
2023-10-01 00:28:25 +0000

    Sorta there now

--- rust/post_generator/src/main.rs
+++ rust/post_generator/src/main.rs
@@ -3,10 +3,37 @@ use std::{net::SocketAddr, sync::Arc};
 use axum::{
     extract::{Json, State},
     http::StatusCode,
+    response::IntoResponse,
     routing::post,
     Router, Server,
 };
 use post_generator::*;
+
+type JsonHandlerResponse<T> = Result<Json<T>, JsonErrResponse>;
+pub struct JsonErrResponse(pub StatusCode, pub Json<HttpErrResponse>);
+impl IntoResponse for JsonErrResponse {
+    fn into_response(self) -> axum::response::Response {
+        (self.0, self.1).into_response()
+    }
+}
+
+impl From<GeneratorError> for JsonErrResponse {
+    fn from(err: GeneratorError) -> Self {
+        let status_code = match err {
+            GeneratorError::EnvMissing(_) => StatusCode::INTERNAL_SERVER_ERROR,
+            GeneratorError::MissingMetaData
+            | GeneratorError::ParseMdError(_)
+            | GeneratorError::ParseMetadataError(_) => StatusCode::BAD_REQUEST,
+        };
+
+        JsonErrResponse(
+            status_code,
+            Json(HttpErrResponse {
+                error: err.to_string(),
+            }),
+        )
+    }
+}
 
 struct GeneratorState {
     config: GeneratorConfig,
@@ -47,14 +74,15 @@ async fn main() {
     println!("Listening on {}", socket_addr);
 }
 
-type JsonHandlerResponse<T> = Result<Json<T>, (StatusCode, Json<HttpErrResponse>)>;
 
 async fn handle(
     State(state): State<Arc<GeneratorState>>,
     Json(payload): Json<GeneratePostRequest>,
 ) -> JsonHandlerResponse<GeneratePostResponse> {
     let config = state.config.clone();
-    let generate_result = generate_post(&payload.markdown_str, config.into());
+    let generate_result = generate_post(&payload.markdown_str, config.into())?;
 
-    todo!()
+    Ok(Json(GeneratePostResponse {
+        generated_post: generate_result,
+    }))
 }
