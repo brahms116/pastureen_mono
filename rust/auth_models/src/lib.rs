9fcf590f046a7cc326d48a0b9d88c0bc8d39118f
David Kwong <20544dk@gmail.com>
2023-10-03 07:29:47 +1000

    Make auth_service_models

--- rust/auth_models/src/lib.rs
+++ rust/auth_models/src/lib.rs
@@ -47,26 +47,4 @@ pub struct TokenPair {
     pub refresh_token: String,
 }
 
-// SERVICE_CONTRACTS
-
-/// A response from the Auth Service to a request for a user
-#[derive(Serialize, Deserialize, Debug, Clone)]
-pub struct GetUserResponse {
-    /// The the requested user
-    pub user: User,
-}
-
-/// Request to retrieve a token pair from logging in
-#[derive(Serialize, Deserialize, Debug, Clone)]
-pub struct LoginRequest {
-    pub email: String,
-    pub password: String,
-}
-
-/// Response from the Auth Service to a request for a token pair
-#[derive(Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
-pub struct TokenPairResponse {
-    pub token_pair: TokenPair,
-}
 
---
+++ rust/auth_service_models/src/lib.rs
@@ -0,0 +1,26 @@
+use serde::{Deserialize, Serialize};
+use auth_models::*;
+
+
+// SERVICE_CONTRACTS
+
+/// A response from the Auth Service to a request for a user
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct GetUserResponse {
+    /// The the requested user
+    pub user: User,
+}
+
+/// Request to retrieve a token pair from logging in
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct LoginRequest {
+    pub email: String,
+    pub password: String,
+}
+
+/// Response from the Auth Service to a request for a token pair
+#[derive(Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct TokenPairResponse {
+    pub token_pair: TokenPair,
+}
