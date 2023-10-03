a22fe507eb13169244b6d7a3f5d071007864eb24
David Kwong <20544dk@gmail.com>
2023-10-04 07:30:50 +1000

    Golang auth models

---
+++ golang/auth_models/main.go
@@ -0,0 +1,33 @@
+package main
+
+type User struct {
+	Fname string `json:"fname"`
+	Lname string `json:"lname"`
+	Email string `json:"email"`
+}
+
+type Claims struct {
+	Sub       string `json:"sub"`
+	Exp       int64  `json:"exp"`
+	Iat       int64  `json:"iat"`
+	TokenType string `json:"tokenType"`
+	Id        string `json:"id"`
+}
+
+type TokenPair struct {
+	AccessToken  string `json:"accessToken"`
+	RefreshToken string `json:"refreshToken"`
+}
+
+type GetUserReponse struct {
+	User User `json:"user"`
+}
+
+type LoginRequest struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+type TokenPairResponse struct {
+	TokenPair TokenPair `json:"tokenPair"`
+}
