91aaf936127278c0d31e20cfa110041fad1d8bf9
David Kwong <20544dk@gmail.com>
2023-10-28 18:27:00 +1000

    New permissions for librarian

--- golang/librarian/terraform/module/main.tf
+++ golang/librarian/terraform/module/main.tf
@@ -1,7 +1,39 @@
 module "function" {
-  source               = "../../../../terraform/aws_lambda"
-  lambda_function_name = "${local.service}_${var.environment}"
-  http_adapter         = true
+  source                       = "../../../../terraform/aws_lambda"
+  lambda_function_name         = "${local.service}_${var.environment}"
+  http_adapter                 = true
+  lambda_execution_role_policy = aws_iam_policy.librarian_policy.arn
+}
+
+resource "aws_iam_role_policy" "librarian_policy" {
+  name = "librarian_policy"
+  role = module.function.lambda_role_name
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "s3:*",
+        ]
+        Effect = "Allow"
+        Resource = [
+          "arn:aws:s3:::pastureen-blog-${var.environment}",
+          "arn:aws:s3:::pastureen-blog-${var.environment}/*",
+        ]
+      },
+      {
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ]
+        Effect = "Allow"
+        Resource = "*"
+      }
+    ]
+  })
+
 }
 
 resource "aws_lambda_function_url" "lambda_url" {
--- golang/librarian/terraform/module/variables.tf
+++ golang/librarian/terraform/module/variables.tf
@@ -1,7 +1,7 @@
 variable "environment" {
   description = "The environment to deploy to"
   validation {
-    condition = var.environment == "dev" || var.environment == "prod" || var.environment == "test"
+    condition     = var.environment == "dev" || var.environment == "prod" || var.environment == "test"
     error_message = "Environment must be one of dev, prod, or test"
   }
 }
