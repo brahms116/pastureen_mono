6b2932ac0c11632ee61a0d9146159d500737d436
David Kwong <20544dk@gmail.com>
2023-11-05 15:38:52 +1000

    make override envs more lax

--- scripts/local_template.sh
+++ scripts/local_template.sh
@@ -24,25 +24,13 @@ while read line; do
   for required_env in "${required_envs[@]}"; do
     if [[ "$required_env" == "$key" ]]; then
       export "$line"
+      break
     fi
   done
 done < "../../.local.env"
 
 if [[ -e "./.overrides.env" ]]; then
-  while read line; do
-    # Check it doesn't start with a #
-    [[ $line =~ ^#.*$ ]] && continue
-
-    # Check it's not empty
-    [[ -z "$line" ]] && continue
-
-    key=$(echo "$line" | sed -E 's/(^[^=]+)=.*/\1/')
-
-    # Set the environment variable if required
-    for required_env in "${required_envs[@]}"; do
-      if [[ "$required_env" == "$key" ]]; then
-        export "$line"
-      fi
-    done
-  done < "./.overrides.env"
+  # No validation here, just override the envs as some envs like AWS_* shouldn't belong
+  # in the required_envs file
+  source "./.overrides.env"
 fi
